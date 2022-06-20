c47b3b964f7adef38449b12110ea5e3d5e55764b
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-06-20 21:14:31 +0800

    update notes

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/XiaoqingLee/LearningGo
 
-go 1.17
+go 1.18
 
 require github.com/biter777/countries v1.3.4 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import (
-	"github.com/XiaoqingLee/LearningGo/chapters/ch6"
-)
+import "fmt"
 
 func main() {
-	ch6.TestVarInitialization()
+	array1 := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+
+	fmt.Printf("array1: %#v\n", array1[len(array1):])
+	fmt.Printf("array1: %#v\n", array1[len(array1):len(array1)])
+
+	fmt.Printf("array1: %#v\n", array1[:0])
+	fmt.Printf("array1: %#v\n", array1[0:0])
 }
